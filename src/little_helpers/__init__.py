b0bef4e7cb144cabec494d6e55ca85d9f4b00324
Snijderfrey <peter.vandersnijderfrey@web.de>
2021-10-18 21:16:52 +0200

    Adjusted for packaging

--- src/little_helpers/__init__.py
+++ src/little_helpers/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from . import math_functions
+from . import array_tools
+from . import num_derive
 from .statsmodel_wrapper import statsmodel_wrapper
 from .num_derive import derivative
 
