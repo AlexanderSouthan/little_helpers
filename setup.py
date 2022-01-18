f411e0ef0fadca31787968578ee2e9b3f543e4c1
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2022-01-18 13:14:33 +0100

    Increased version number for next upload to PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='little_helpers',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(where='src'),
     install_requires=['numpy', 'scikit-learn', 'scipy', 'statsmodels']
 )
