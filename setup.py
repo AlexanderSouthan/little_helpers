0e395cb50fd31c03598aa2206899ad086178effc
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2022-01-27 21:47:57 +0100

    Changed to new version 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='little_helpers',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(where='src'),
     install_requires=['numpy', 'scikit-learn', 'scipy', 'statsmodels', 'sympy', 'python-docx', 'latex2mathml', 'lxml']
 )
