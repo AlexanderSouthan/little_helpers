7a3ea3b3c56a5cf2b78582bae15eeb2c8e6b3832
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2023-04-14 22:40:56 +0200

    Increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='little_helpers',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(where='src'),
     install_requires=['numpy', 'scikit-learn', 'scipy', 'statsmodels', 'matplotlib', 'sympy', 'python-docx', 'latex2mathml', 'lxml']
 )
