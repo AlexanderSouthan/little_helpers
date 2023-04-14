6d8ed8b66909ca7d53bd689fcb3f756cebd59c81
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2023-04-14 22:40:32 +0200

    Added ols wrapper

--- src/little_helpers/statsmodel_wrapper.py
+++ src/little_helpers/statsmodel_wrapper.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import statsmodels.api as sm
+from statsmodels.formula.api import ols
 from sklearn.base import BaseEstimator, RegressorMixin
 
 class statsmodel_wrapper(BaseEstimator, RegressorMixin):
@@ -19,4 +20,15 @@ class statsmodel_wrapper(BaseEstimator, RegressorMixin):
     def predict(self, X):
         if self.fit_intercept:
             X = sm.add_constant(X)
+        return self.results_.predict(X)
+
+class ols_wrapper(BaseEstimator, RegressorMixin):
+    def __init__(self, model_string, response_name):
+        self.model_string = model_string
+        self.response_name = response_name
+    def fit(self, X, y):
+        X[self.response_name] = y
+        self.model_ = ols(self.model_string, X)
+        self.results_ = self.model_.fit()
+    def predict(self, X):
         return self.results_.predict(X)
